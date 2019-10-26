fc07b80ef8e831d209eaf0a4de7de1a1b3b55bbf
ivanfl190 <54650673+ivanfl190@users.noreply.github.com>
2019-10-25 23:09:33 -0300

    Update banco.sh

--- Aula 3/banco.sh
+++ Aula 3/banco.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
 sudo yum -y update
+sudo yum -y install wget
 sudo yum -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 sudo wget https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-centos96-9.6-3.noarch.rpm
 sudo yum -y install pgdg-centos96-9.6-3.noarch.rpm epel-release
@@ -8,7 +8,16 @@ sudo yum -y install postgresql96-server postgresql96-contrib
 sudo /usr/pgsql-9.6/bin/postgresql96-setup initdb
 sudo systemctl start postgresql-9.6
 sudo systemctl enable postgresql-9.6
+sudo chmod -R 777 /home/vagrant
+sudo -u postgres psql postgres 
+sudo -u postgres createuser vagrant
+sudo -u postgres createuser root
+sudo su postgres
+createdb mydb
+psql mydb
 
+#sudo su --shell /bin/bash postgres
+#sudo su postgres psql
 
 
 
